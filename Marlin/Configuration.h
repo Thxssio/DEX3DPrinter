712127d49be786a51a565cf8fd40b1299fe2e89f
Thassio <95764952+Thxssio@users.noreply.github.com>
2023-03-24 03:52:42 -0300

    Update Configuration.h
    Edit names

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -60,7 +60,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(thxssio, Graber i3)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(thxssio, DEX 3D Printer i3)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
@@ -135,7 +135,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "3D DEX"
+#define CUSTOM_MACHINE_NAME "DEX 3D Printer"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
